317ff5efc22b2259c4ad3e56555086b66ef4c8ff
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-07 18:38:41 +0000

    added in html, CSS and JS files

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//need to star the quiz. 
+//store answers in variables
+//have the timer function ready
+
+//timer starts on go button. 
+//change question
+//load the 4 answers. 
+
+//if answer is wrong take away 10s
+//if answer is right move on. 
+
+//store the score
+//have a highscore page ready to load anytime. (do I make this on a new page?)#
+
+//questions
+
+var questionOne = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+var questionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+var questionThree = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+var questionfour = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+
+//question one answers 
+
+var answerOneONe = 'Array'
+var answerOneTwo = 'Javascript'
+var answerOneThree = 'Function'
+var answerOneFour = 'Cascade'
+
+//load in question one when start is clicked. text.content. and show buttons? 
+
+function createquestion () {
+
+}
