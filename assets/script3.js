16354a621f0c3724742ce42f6d7148a638a71706
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-08 21:46:08 +0000

    addedfiled

--- assets/script3.js
+++ assets/script3.js
@@ -33,39 +33,114 @@ let startButton = document.getElementById('start__btn')
 let questionButton = document.getElementById('questioncontainer')
 let startContainer = document.getElementById('startcontainerid')
 
-//===================================
-// ================================== Event Listener
 
-startButton.addEventListener('click', startGame);
 
 //===================================
 //=================================== Start Game Function
 
 function startGame () {
     console.log('started')
-    startButton.classList.add('hide');
+    // startButton.classList.add('hide');
     questionButton.classList.remove('hide');
     startContainer.classList.add('hide');
 
-    // timerId = setInterval(clockTick, 1000)
-    // showQuestion() clock is broken for some reason
+    // timerId = setInterval(clockTick, 1000) clock was broken
+    showQuestion();
 }
 
 //===========================================
 //=========================================== Show question function
 
+//method 1
+
+// function showQuestion () {
+//     var currentQs = questionlist[currentQsIndex]
+//     questionElement.innerText = currentQs.question;
+//     for (var i = 0; i < currentQs.length; i++) {
+//     //     console.log('insidecreatebutton')
+//     //     const button = document.createElement('button')
+//     //     button.innerText = currentQs.answers[i]
+//     //     button.classList.add('btn')
+//     //     button.addEventListener('click', selectAnswer)
+//     //     answerButtonsElement.appendChild(button);
+//     // } 
+
+// ============================================================= why does this not work?
+
+// ============================================================== Method 2
+
+// function showQuestion () {
+//     //     var currentQs = questionlist[currentQsIndex]
+//     //     questionElement.innerText = currentQs.question;
+//     questionlist.answers.forEach(answer => {
+//         const button = document.createElement('button')
+//         button.innerText = answer.textContent
+//         button.classList.add('btn')
+//         button.addEventListener('click', SelectAnswer)
+//         answerButtonsElement.appendChild(button)
+//     })
+
+// ========================================================================why does this not work?
+
+
 
 function showQuestion () {
     var currentQs = questionlist[currentQsIndex]
     questionElement.innerText = currentQs.question;
-    questionlist.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
+    currentQs.answers.forEach(function (answers, i) {
+    
+        var currentChoice = document.createElement('button');
+        currentChoice.setAttribute('class','btn');
+        currentChoice.setAttribute('value', answers);
+        currentChoice.textContent = i+1+'. '+ answers;
+        currentChoice.addEventListener('click', checkanswer);
+        answerButtonsElement.appendChild(currentChoice);
     })
- }
- 
+}
 
- 
+function checkanswer() {
+    console.log('checkanswer');
+}
+
+// ================================================================================
+//  ===============================================================================//function for clicking on the questions
+
+let seconds = 60
+let score = 0
+let incorrect = 0
+
+
+function checkAnswer(){
+
+    if(this.value !== question[currentQsIndex].answer){
+
+        alert('correct')
+        score += 1
+    } else {
+        alert('incorrect')
+        score -=1
+    }
+
+    currentQsIndex++
+    if(currentQsIndex === question.length){
+        endGame()
+    } else {
+        question()
+    }
+}
+
+
+
+function setTimer(){
+
+    timercountdowntext.textContent = ""
+
+
+
+}
+
+// ============================================//need a function to render results. 
+//===================================
+// ================================== Event Listener
+
+startButton.addEventListener('click', startGame);
