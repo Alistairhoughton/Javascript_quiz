082cda56dcab84cf0ef688603718eb5dc0bef24b
Alistair Houghton <alistair.g.houghton@gmail.com>
2021-11-08 16:26:05 +0000

    updated js

--- assets/script.js
+++ assets/script.js
@@ -14,20 +14,80 @@
 
 //questions
 
-var questionOne = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
-var questionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
-var questionThree = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
-var questionfour = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+
+//this hid my text for some reason??????
+
+// document.querySelector('questiontext'); //use this to change the text in the doc 
+// questiontext = document.getElementById('questiontext');
+// questiontext.textContent = questionOne;
+
+// need a score 10 points for a right answer, -5 points for a wrong answer. starts at 0. is it a string or array?
+
+// var questionOne = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+// var questionTwo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+// var questionThree = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
+// var questionfour = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. ";
 
 //question one answers 
 
-var answerOneONe = 'Array'
-var answerOneTwo = 'Javascript'
-var answerOneThree = 'Function'
-var answerOneFour = 'Cascade'
+// var answerOneONe = 'Array';
+// var answerOneTwo = 'Javascript';
+// var answerOneThree = 'Function';
+// var answerOneFour = 'Cascade';
+
+// var rightAnswer = true;
+// var wrongAnswer = false;
+
+var score = ('0');
+
+
+var questionElement = document.getElementById('questiontext')
+var answerButtonsElement = document.getElementById('answer--btn')
+
+
+
+var questionlist = [
+    {
+        question: "what is 2+2",
+        answers:[
+            {text:'4', correct: true},
+            {text:'22', incorrest: false} //idk if this is a boolean????
+        ]
+    }
+]
+
+console.log(questionlist);
 
 //load in question one when start is clicked. text.content. and show buttons? 
 
-function createquestion () {
+//when start button is clicked, bring in the question and the answers. 
+//I think i need a .value as this value property sets or returns the value of the value attribute of a text field.
 
+let startButton = document.getElementById('startcontainerid')
+let questionButton = document.getElementById('questioncontainer')
+
+startButton.addEventListener('click', startGame);
+
+function startGame () {
+    console.log('started')
+    startButton.classList.add('hide');
+    questionButton.classList.remove('hide');
 }
+
+function showQuestion (question) {
+    questionElement.innerText =  questionlist.question;
+} 
+
+showQuestion()
+
+//I idk if the function below would work. 
+
+// function selectAnswer () {
+// if (document.getElementById(questiontext).value === true)
+// return 'correct';
+
+// else  (document.getElementById(questiontext).value === false)
+// return 'incorrect';
+// }
+
+//if incorrect take off 10 seconds and -5 points and dont move on, if correct load new questions. 
